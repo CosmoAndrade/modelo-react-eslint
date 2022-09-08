33d534eba7218456f00da6bda56446f698743222
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-09-07 21:38:58 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Oka</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Oka</h1>
+        </div>
+    );
 }
 
 export default App;
